74bd4966b9bb43fac9d1cfaf98a1643a338954b1
dymafr <erwan1309@hotmail.com>
2022-02-24 17:57:51 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
-  <button @click="toggle = !toggle">Toggle</button>
-  <h1 v-show="toggle">Bonjout tout le monde !</h1>
-  <h2 v-pre>{{ uneVariable }}</h2>
+  <ul>
+    <li v-for="(fruit, index) in fruits">{{ `${index} - ${fruit}` }}</li>
+  </ul>
+
+  <ul>
+    <li v-for="({ prenom, notes }, index) of utilisateurs">
+      {{ `${index} : ${prenom}` }}
+      <span v-for="note in notes">{{ note }} / </span>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
-const toggle = ref(true);
-const uneVariable = ref('une valeur');
+const fruits = reactive(['fraise', 'pomme', 'kiwi']);
+const utilisateurs = reactive([
+  { prenom: 'Jean', notes: [14, 15, 18] },
+  { prenom: 'Paul', notes: [12, 10, 8] },
+  { prenom: 'Pierre', notes: [3, 15, 18] },
+]);
 </script>
 
 <style scoped lang="scss"></style>
