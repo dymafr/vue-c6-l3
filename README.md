dcad46e79da9b98a6dcde523e0c25e8b9c0ffb36
dymafr <erwan1309@hotmail.com>
2022-02-24 17:48:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-5w4emv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-5w4emv)
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="toggle = !toggle">Toggle</button>
+  <h1 v-show="toggle">Bonjout tout le monde !</h1>
+  <h2 v-pre>{{ uneVariable }}</h2>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const toggle = ref(true);
+const uneVariable = ref('une valeur');
+</script>
+
+<style scoped lang="scss"></style>
